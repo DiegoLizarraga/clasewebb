a0641a337e348e1b05509e6f53589c62929e46a3
DiegoLizarraga <lizarragasanchezdiego@gmail.com>
2026-03-23 20:57:03 -0600

    entrega de clase

--- script/script.js
+++ script/script.js
@@ -1,28 +1,45 @@
-let fullop =  '';
+let fullop = '';
 
 function handleClick(number) {
   console.log(number);
 
-  fullop = fullop + number; 
+  fullop = fullop + number;
   showNumber(fullop);
 }
 
-function calculate(){
-  console.log({fullop});
-  const a= fullop.split(/(\+|-|x|\/)/gm)
-  console.log(( a, op, b));
+function calculate() {
+  console.log({ fullop });
+  const [a, op, b] = fullop.split(/(\+|-|x|\/)/gm);
+  console.log({ a, op, b });
+
+  let res;
+
   switch (op) {
-    case "1":
-      res = Number(a) = Number(b);
-    break
-    
+    case '+':
+      res = Number(a) + Number(b);
+      break;
+    case '-':
+      res = Number(a) - Number(b);
+      break;
+    case 'x':
+      res = Number(a) * Number(b);
+      break;
+    case '/':
+      res = Number(a) / Number(b);
+      break;
     default:
-
-    break
-
+      break;
   }
+
+  fullop = String(res);
+  showNumber(res);
 }
 
-function showNumber(n){
+function handleClear() {
+  fullop = '';
+  showNumber('0');
+}
+
+function showNumber(n) {
   document.getElementById('screen').innerHTML = n;
 }
